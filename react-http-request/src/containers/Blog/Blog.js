0dd5c21a5290fb16cdef491a94e69994078b3a71
John <jkforsythe@hotmail.com>
2020-06-09 23:33:10 +0100

    updated response

--- react-http-request/src/containers/Blog/Blog.js
+++ react-http-request/src/containers/Blog/Blog.js
@@ -8,7 +8,10 @@ import './Blog.css';
 
 class Blog extends Component {
     componentDidMount () {
-
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response);
+            });
     }
 
     render () {
