bad4ec6bb78e3dbffca0250bfcd286ec15221dc2
John <jkforsythe@hotmail.com>
2020-06-14 08:10:41 +0100

    posts functions moved into folder

--- react-http-request/src/containers/Blog/Blog.js
+++ react-http-request/src/containers/Blog/Blog.js
@@ -2,17 +2,9 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from './FullPost/FullPost';
-import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
 
     componentDidMount () {
         axios.get('/posts')
@@ -33,22 +25,7 @@ class Blog extends Component {
             });
     }
 
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                        return <Post 
-                            key={post.id} 
-                            title={post.title} 
-                            author={post.author}
-                            clicked={() => this.postSelectedHandler(post.id)}/>;
-                        });
-        }
-
         return (
             <div>
                 <header className="Blog">
@@ -59,9 +36,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                {posts}
-                </section>
+                
             </div>
         );
     }
---
+++ react-http-request/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Posts extends Component {
+    state = {
+      posts: [],
+  }
+
+    postSelectedHandler = (id) => {
+      this.setState({selectedPostId: id});
+  }
+
+    render() {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                        return <Post 
+                            key={post.id} 
+                            title={post.title} 
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)}/>;
+                        });
+
+      return (
+        <section className="Posts">
+        {posts}
+        </section>
+      )
+    }
+  }
+}
