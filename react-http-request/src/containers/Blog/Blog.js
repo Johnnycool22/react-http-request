213c8d5be536df6fa649100ce60b41ffbad53470
John <jkforsythe@hotmail.com>
2020-06-18 22:01:31 +0100

    props match url added for nested route

--- react-http-request/src/containers/Blog/Blog.js
+++ react-http-request/src/containers/Blog/Blog.js
@@ -31,9 +31,10 @@ class Blog extends Component {
                 </header>
                     {/*<Route path="/" exact render={() => <h1>Home</h1>} />
                     <Route path="/" render={() => <h1>Home2</h1>} />*/}
-                    <Route path="/" exact component={Posts}/>
+                    
                     <Switch>
                         <Route path="/new-post" component={NewPost}/>
+                        <Route path="/" component={Posts}/>
                     </Switch>
             </div>
         );
