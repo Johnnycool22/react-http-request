5502f0ab0723b6a618c4534d5bde83c4e09b30f5
John <jkforsythe@hotmail.com>
2020-06-18 21:51:16 +0100

    nested route

--- react-http-request/src/containers/Blog/Blog.js
+++ react-http-request/src/containers/Blog/Blog.js
@@ -4,7 +4,6 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -35,9 +34,7 @@ class Blog extends Component {
                     <Route path="/" exact component={Posts}/>
                     <Switch>
                         <Route path="/new-post" component={NewPost}/>
-                        <Route path="/:id" exact component={FullPost}/>
                     </Switch>
-                    
             </div>
         );
     }
